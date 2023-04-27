984efe1aa290dec2cda68b6678dc93c21451a04d
Khuzema <maslam002@citymail.cuny.edu>
2023-04-27 18:39:10 -0400

    Added questions prompts. The .then and .catch are next

--- server.js
+++ server.js
@@ -3,10 +3,44 @@ const mysql = require('mysql2');
 const inquirer = require('inquirer');
 
 
-const dBase = require('./db/connection');
 
-dBase.connect(err => {
-    if(err) throw err;
-    console.log('Database connected');
+const dBase = require('./db/connections');
 
+dBase.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to database!');
+    tracker();
+    // test();
 });
+
+var tracker = function() {
+inquirer.prompt([{
+    // Begin Command Line
+    type: 'list',
+    name: 'prompt',
+    message: 'What would you like to do?',
+    choices: ['View All Employees','Add An Employee','Update An Employee Role','View All Roles','Add Role', 'View All Department','Add Department', 'Leave']
+}]);
+
+}
+// var test = function(){
+// inquirer.prompt([
+//     {
+//       type: 'input',
+//       name: 'name',
+//       message: 'What is your name?'
+//     },
+//     {
+//       type: 'list',
+//       name: 'color',
+//       message: 'What is your favorite color?',
+//       choices: ['Red', 'Green', 'Blue']
+//     }
+//   ])
+//   .then((answers) => {
+//     console.log(`Hello ${answers.name}, your favorite color is ${answers.color}.`);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+// }
